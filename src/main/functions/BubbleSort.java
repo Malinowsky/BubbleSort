228d1f33330a25dcb193614ffb8ec69d596d3eac
Artur <arturrmalinowski@gmail.com>
2022-06-05 21:28:05 +0200

    Changes

--- src/main/functions/BubbleSort.java
+++ src/main/functions/BubbleSort.java
@@ -6,50 +6,36 @@ import java.util.List;
 
 
 public class BubbleSort {
-    public static ArrayList<Double> sort(ArrayList<Double> array, int length){
-        for(int i = 0; i<length-1; ++i){
+    public ArrayList<Double> sort(ArrayList<Double> input){
+        if (input != null) {
 
-            for(int j = 0; j<length-i-1; ++j){
+            sortedAscending(input);
 
-                if(array.get(j) > array.get(j + 1)){
-                    double temp = array.get(j);
-                    array.set(j, array.get(j + 1));
-                    array.set(j + 1, temp);
+        }else if(input.contains(input)){
+
+            while (input.remove(null));
+
+            sortedAscending(input);
+        }else{
+
+            throw new RuntimeException("RuntimeException");
+
+        }
+        return input;
+    }
+
+    private void sortedAscending(ArrayList<Double> input) {
+        for (int i = 0; i < input.size() - 1; ++i) {
+
+            for (int j = 0; j < input.size() - i - 1; ++j) {
+
+                if (input.get(j) > input.get(j + 1)) {
+
+                    double temp = input.get(j);
+                    input.set(j, input.get(j + 1));
+                    input.set(j + 1, temp);
                 }
             }
         }
-        return array;
-    }
-
-    private static void showAll(ArrayList<Double> array, int length){
-        for (int i = 0; i < length; ++i) {
-            System.out.print(array.get(i) + " ");
-        }
-    }
-
-    private static ArrayList<Double> run(List<Double> input){
-        try {
-           input = new ArrayList<>(
-                    Arrays.asList(null)
-            );
-
-            int length = input.size();
-
-            sort((ArrayList<Double>) input, length);
-            showAll((ArrayList<Double>) input,length);
-
-
-        } catch (NullPointerException e) {
-            while (input.remove(null));
-            System.out.print(input);
-        } catch (RuntimeException e){
-            e.getMessage();
-        }
-        return (ArrayList<Double>) input;
-    }
-
-    public static void main( String args[] ) {
-        List<Double> input = null;
-        run(input);
     }
 }
--- test/BubbleSortTests.java
+++ test/BubbleSortTests.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 import static org.junit.Assert.assertEquals;
 
 public class BubbleSortTests {
+
 
     @Test
     public void allIntegers(){
@@ -16,48 +18,46 @@ public class BubbleSortTests {
         ArrayList<Double> input = new ArrayList<>(
                 Arrays.asList(1.0,4.0,5.0,6.0,8.0,3.0,8.0)
         );
-        int size = input.size();
 
         BubbleSort instance = new BubbleSort();
 
         ArrayList<Double> expResult = new ArrayList<>(
                 Arrays.asList(1.0,3.0,4.0,5.0,6.0,8.0,8.0)
         );
-        ArrayList<Double> result = instance.sort(input,size);
+        ArrayList<Double> result = instance.sort(input);
         assertEquals(result,expResult);
-        System.out.println("Test is successful");
+        System.out.println("1.input = [1,4,5,6,8,3,8] result = [1,3,4,5,6,8,8]");
     }
     @Test
     public void notAllIntegers(){
         ArrayList<Double> input = new ArrayList<>(
                 Arrays.asList(-9.3,0.2,4.0,0.1,5.0,9.0)
         );
-        int size = input.size();
 
         BubbleSort instance = new BubbleSort();
 
         ArrayList<Double> expResult = new ArrayList<>(
                 Arrays.asList(-9.3,0.1,0.2,4.0,5.0,9.0)
         );
-        ArrayList<Double> result = instance.sort(input,size);
+        ArrayList<Double> result = instance.sort(input);
         assertEquals(result,expResult);
-        System.out.println("Test is successful");
+        System.out.println("2.input = [-9.3,0.2,4,0.1,5,9] result = [-9.3,0.1,0.2,4,5,9]");
     }
     @Test
     public void emptyArray(){
         ArrayList<Double> input = new ArrayList<>(
                 Arrays.asList()
         );
-        int size = input.size();
+
 
         BubbleSort instance = new BubbleSort();
 
         ArrayList<Double> expResult = new ArrayList<>(
                 Arrays.asList()
         );
-        ArrayList<Double> result = instance.sort(input,size);
+        ArrayList<Double> result = instance.sort(input);
         assertEquals(result,expResult);
-        System.out.println("Test is successful");
+        System.out.println("3.input = [] result = []");
     }
 
 
@@ -68,7 +68,7 @@ public class BubbleSortTests {
                 Arrays.asList(null,5.0001)
         );
         while (input.remove(null));
-        int size = input.size();
+
 
         BubbleSort instance = new BubbleSort();
 
@@ -76,10 +76,10 @@ public class BubbleSortTests {
                 Arrays.asList(5.0001)
         );
 
-        ArrayList<Double> result = instance.sort(input,size);
+        ArrayList<Double> result = instance.sort(input);
 
         assertEquals(result,expResult);
-        System.out.println("Test is successful");
+        System.out.println("4.input = [null,5.0001] result = [5.0001]");
     }
     @Test
     public void onlyNull (){
@@ -87,12 +87,12 @@ public class BubbleSortTests {
             ArrayList<Double> input = new ArrayList<>(
                     Arrays.asList(null)
             );
-            int size = input.size();
-        }catch (RuntimeException e){
-            RuntimeException expResult = null;
-            assertEquals(e,expResult);
-        }
 
-        System.out.println("Test is successful");
+            RuntimeException expResult = null;
+
+            assertEquals(input, expResult);
+        }catch (RuntimeException e){
+            System.out.println("5.input = null   result = "+e.getMessage());
+        }
     }
 }
